dcd1773d9dbd748f9ed9660098297d17f6574fd1
febc.yamamoto <yamamoto.febc@gmail.com>
2016-01-22 11:10:00 +0900

    fix #14 Add plugin version option

--- cmd/docker-machine-driver-sakuracloud.go
+++ cmd/docker-machine-driver-sakuracloud.go
@@ -1,10 +1,43 @@
 package main
 
 import (
+	"os"
+	"path"
+
+	"github.com/codegangsta/cli"
 	"github.com/docker/machine/libmachine/drivers/plugin"
 	"github.com/yamamoto-febc/docker-machine-sakuracloud"
+	"github.com/yamamoto-febc/docker-machine-sakuracloud/version"
 )
 
+var appHelpTemplate = `This is a Docker Machine plugin for SAKURA CLOUD.
+Plugin binaries are not intended to be invoked directly.
+Please use this plugin through the main 'docker-machine' binary.
+Version: {{.Version}}{{if or .Author .Email}}
+Author:{{if .Author}}
+  {{.Author}}{{if .Email}} - <{{.Email}}>{{end}}{{else}}
+  {{.Email}}{{end}}{{end}}
+{{if .Flags}}
+Options:
+  {{range .Flags}}{{.}}
+  {{end}}{{end}}
+Commands:
+  {{range .Commands}}{{.Name}}{{with .ShortName}}, {{.}}{{end}}{{ "\t" }}{{.Usage}}
+  {{end}}
+`
+
 func main() {
-	plugin.RegisterDriver(sakuracloud.NewDriver("", ""))
+
+	cli.AppHelpTemplate = appHelpTemplate
+	app := cli.NewApp()
+	app.Name = path.Base(os.Args[0])
+	app.Usage = "This is a Docker Machine plugin binary. Please use it through the main 'docker-machine' binary."
+	app.Author = "Kazumichi Yamamoto(yamamoto.febc@gmail.com)"
+	app.Email = "https://github.com/yamamoto-febc/docker-machine-sakuracloud/"
+	app.Version = version.FullVersion()
+	app.Action = func(c *cli.Context) {
+		plugin.RegisterDriver(sakuracloud.NewDriver("", ""))
+	}
+
+	app.Run(os.Args)
 }
--- version/version.go
+++ version/version.go
@@ -1,6 +1,15 @@
 package version
 
+import "fmt"
+
 var (
-	Version   = "0.0.7"
+	// Version return version no
+	Version = "0.0.7"
+	// GitCommit return Git commit
 	GitCommit = "HEAD"
 )
+
+// FullVersion return full version string
+func FullVersion() string {
+	return fmt.Sprintf("%s, build %s", Version, GitCommit)
+}
